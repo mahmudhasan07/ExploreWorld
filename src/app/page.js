83ffed4e158d6edc1a7e86685652a871e59177d9
unknown <mahmudhasan.hb@gmail.com>
2024-10-06 02:01:04 +0600

    added

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,15 @@ import TopReview from "./components/TopReview/TopReview";
 export const metadata = {
   title: "ExploreSphere Travel",
   description: "Explore the world with ExploreSphere Travel",
+  openGraph: {
+    images: [{
+      url: 'https://res.cloudinary.com/daudgshta/image/upload/v1728155408/exploreworld_zxxy0k.png'
+    }]
+  },
   icons: {
     icon: "https://i.ibb.co/f9g36Zz/exploresphere-high-resolution-logo-transparent-1.png"
   },
-  openGraph: {
-    images: "https://res.cloudinary.com/daudgshta/image/upload/v1728155408/exploreworld_zxxy0k.png"
-  }
+
 
 }
 
