ee11645f0de6f71fe00199fac8ee67ca3c4aaf2b
unknown <mahmudhasan.hb@gmail.com>
2024-10-06 01:39:59 +0600

    working on profile SSR

--- src/app/all-blogs/[card]/page.js
+++ src/app/all-blogs/[card]/page.js
@@ -4,24 +4,24 @@ import CardDetails from "./CardDetails";
 import '../../components/lifestyle/Lifestyle.scss'
 
 
-export const generateMetadata = async ({params}) => {
+export const generateMetadata = async ({ params }) => {
     const axiosLink = useAxios(AxiosSource)
     const cards = await axiosLink.get(`/blogs/${params.card}`)
-    return{
-        title : cards.data.name,
-        openGraph : {
-            images : cards.data.hostImages[0]
+    return {
+        title: cards.data.name,
+        openGraph: {
+            images: [{ url: cards.data.hostImages[0] }]
         },
-        icons :{
-            icon : cards.data.hostImages[0]
+        icons: {
+            icon: cards.data.hostImages[0]
         }
 
-        
+
     }
 }
 
-export default function Page({params})   {
-    
+export default function Page({ params }) {
+
     return (
         <section className="my-5">
             <h1 id='title' className="text-4xl font-bold text-center mb-10">Blog Details</h1>
