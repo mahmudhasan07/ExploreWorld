dc2c3478df9cc41829cc12d7ba93aec2ef5e0e2f
unknown <mahmudhasan.hb@gmail.com>
2024-10-06 02:11:44 +0600

    working image optimize

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-
         remotePatterns: [
             {
                 protocol: 'https',
-                unoptimized: true,
+                hostname: '**',
             }
-        ]
+        ],
     },
 };
 
