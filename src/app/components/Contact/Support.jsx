f2c6cf487f4df327832231e911effdc556a6fb27
unknown <mahmudhasan.hb@gmail.com>
2024-10-07 01:43:45 +0600

    added framer motion

--- src/app/components/Contact/Support.jsx
+++ src/app/components/Contact/Support.jsx
@@ -1,21 +1,49 @@
 "use client"
-import React from 'react';
+import React, { useRef } from 'react';
 import support from "../../../../public/support.json";
 import Lottie from "lottie-react";
 import "./Support.scss"
+import { useInView } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Support = () => {
+
+    const ref = useRef()
+    const isInView = useInView(ref);
+
     return (
         <section className='my-10'>
             <h1 id='title' className='text-5xl text-center my-5 font-extrabold'>Technical Support </h1>
             <p className='title-text text-base font-semibold text-center my-2'>If you are have any technical issue with our website, please let us know.</p>
             <p className='title-text text-base font-semibold text-center my-1'>We try to solve the problem as soon as possible</p>
 
-            <div className='flex justify-around my-5'>
-                <div className='w-2/5'>
+            <div className='flex justify-around my-5 overflow-hidden'>
+                <motion.div ref={ref}
+                    initial={{ opacity: 0, x: -200 }}
+
+                    animate={isInView ? {
+                        opacity: 1,
+                        x: 0,
+                        transition: { duration: 1.2 },
+                    }
+                        :
+                        {}}
+                    className='w-2/5'>
                     <Lottie animationData={support} loop={true}></Lottie>
-                </div>
-                <div className='p-2'>
+                </motion.div>
+                <motion.div
+                    ref={ref}
+                    initial={{ opacity: 0, x: 200 }}
+
+                    animate={isInView ? {
+                        opacity: 1,
+                        x: 0,
+                        transition: { duration: 1.2 },
+                        
+                    }
+                        :
+                        {}}
+                    className='p-2 overflow-hidden'>
                     <form action="" className='space-y-5'>
                         <div>
                             <label htmlFor="" className='text-xl font-semibold italic'>Enter Your Name :</label> <br />
@@ -33,9 +61,9 @@ const Support = () => {
                             <button id='button_Submit' className='border-2 p-2 text-white mt-3 border-white text-lg rounded-2xl font-semibold'>Submit</button>
                         </div>
                     </form>
-                </div>
+                </motion.div>
             </div>
-            
+
         </section>
     );
 };
--- src/app/components/TopReview/TopReview.jsx
+++ src/app/components/TopReview/TopReview.jsx
@@ -1,10 +1,13 @@
 'use client'
 import useFetch1 from '@/app/Hooks/useFetch1';
-import React from 'react';
+import React, { useRef } from 'react';
+import {motion, useInView} from 'framer-motion'
 import "./TopReview.css"
 
 const TopReview = () => {
     const [data] = useFetch1("topRatting")
+    // const ref = useRef()
+    // const isView = useInView(ref)
     return (
         <section className='my-10'>
             <h1 id='title' className="text-5xl text-center font-bold mb-10">Top Reviews places</h1>
@@ -14,13 +17,16 @@ const TopReview = () => {
                     "loading"
                     :
                     data?.slice(0,4)?.map((e,idx)=> 
-                    <div id='card1' key={idx}>
+                    <motion.div
+                    initial={{opacity : 0, x : -(100*(idx+1))}}
+                    whileInView={{opacity : 1, x : 0, transition :{duration: 0.7*(idx+1)}}}
+                     id='card1' key={idx}>
                         <div id='card2'>
                             <img src={e.hostImages[0]} alt="image" className="w-72 h-52 rounded-2xl"/>
                             <h1 className='text-xl font-semibold'>{e.name}</h1>
                             <h1 className='text-xl font-semibold'>{e.location}</h1>
                         </div>
-                    </div>)
+                    </motion.div>)
                 }
             </div>
         </section>
